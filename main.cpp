271ce070d69c880e965668b7a14b23feec1f9487
Дмитрий Козимиров <d.kozimirov@gmail.com>
2023-11-13 11:26:49 +0300

    added code structure

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <conio.h>
+#include <ctime>
+
+#include "Snake.h"
+#include "Food.h"
+
+#define WIDTH 50
+#define HEIGHT 25
+
+using namespace std;
+
+Snake snake({WIDTH/2, HEIGHT/2}, 1);
+Food food;
+
+int score;
+
+void board() {
+	
+}
 
 int main() {
-
-  std::cout << "Hello world!\n";
-
-  system("pause");
-
-  return 0;
+ 
+return 0;
 
 }
